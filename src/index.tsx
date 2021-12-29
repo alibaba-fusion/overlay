a4249ea8e65c3a67c32d7c551eb6a8be8aa31464
bindoon <bindoon@sina.com>
2021-12-29 17:19:24 +0800

    fix: esc close overlay one by one fix #3606

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import InternalOverlay from './overlay';
 import Popup from './popup';
+import OverlayContext  from './overlay-context';
 
 type InternalOverlayType = typeof InternalOverlay;
 
 interface OverlayInterface extends InternalOverlayType {
   Popup: typeof Popup;
+  OverlayContext: typeof OverlayContext;
 }
 
 const Overlay = InternalOverlay as OverlayInterface;
 Overlay.Popup = Popup;
+Overlay.OverlayContext = OverlayContext;
 
 export default Overlay
